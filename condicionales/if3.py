79f05b72e6c377572142b9f3441818e6ddc81ab7
fernandomartin04 <fernandomartin04@iesamachado.org>
2024-03-20 18:49:42 +0100

    20 mar fin

--- condicionales/if3.py
+++ condicionales/if3.py
@@ -2,6 +2,17 @@
 #Un programa que pida 3 numeros y determine el mayor
 print("Escribe 3 números y te diré el mayor.")
 
-n1=float(input("Introduce el primer número: "))
-n2=float(input("Introduce el segundo número: "))
-n3=float(input("Introduce el tercer número: "))
+n1=int(input("Introduce el primer número: "))
+n2=int(input("Introduce el segundo número: "))
+n3=int(input("Introduce el tercer número: "))
+
+if n1==n2 and n1==n3:
+    print("Todos los número son iguales")
+elif n1>=n2 and n1>=n3:
+    print(f"El número mayor es: {n1}")
+elif n2>=n1 and n2>=n3:
+    print(f"El número mayor es: {n2}")
+elif n3>=n1 and n3>=n2:
+    print(f"El número mayor es: {n3}")
+elif n1==n2 and n1==n3:
+    print("Todos los número son iguales")

a8724334d9b5837ed07fce23b249c52a0d1d95e0
fernandomartin04 <fernandomartin04@iesamachado.org>
2024-04-03 18:28:06 +0200

    camara

--- monedasContorno/contadorMonedasCamara.py
+++ monedasContorno/contadorMonedasCamara.py
@@ -4,6 +4,16 @@ import numpy as np
 
 #Defino funcion
 def ordenarPuntos(puntos):
-    #n_puntos = np.concatenate()
-    saludo = "Hola mundo"
-    print(saludo)
+    #Concateno puntos de la matriz 
+    n_puntos = np.concatenate(puntos[0],puntos[1],puntos[2],puntos[3]).tolist()
+    #Ordeno lo spuntos del eje x e y
+    y_order = sorted(n_puntos,key=lambda n_puntos:n_puntos[1]) #Defino el valor 0
+
+    x1_order = y_order[0:2] #El segundo valor se le resta 1 asiq realmente es 1
+    x1_order = sorted(x1_order, key=lambda x1_order:x1_order[0])
+
+    x2_order = y_order[0:4] #El segundo valor se le resta 1 asiq realmente es 3
+    x2_order = sorted(x1_order, key=lambda x2_order:x2_order[0])
+    return [x1_order[0],x1_order[1],x2_order[0],x2_order[1]]
+
+

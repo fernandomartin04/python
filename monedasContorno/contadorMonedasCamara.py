51a457da7a11fd18cca22093190b5aa12d20df4e
fernandomartin04 <fernandomartin04@iesamachado.org>
2024-04-04 19:35:39 +0200

    continuo camara

--- monedasContorno/contadorMonedasCamara.py
+++ monedasContorno/contadorMonedasCamara.py
@@ -16,4 +16,21 @@ def ordenarPuntos(puntos):
     x2_order = sorted(x1_order, key=lambda x2_order:x2_order[0])
     return [x1_order[0],x1_order[1],x2_order[0],x2_order[1]]
 
-
+def alinemiento(imagen,ancho,alto):
+    imagen_alineada = None
+    grises = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY) #Grises
+    tipo_umbral, umbral = cv2.threshold(grises, 150, 255, cv2.THRESH_BINARY) #Umbral
+    cv2.imshow("Umbral", umbral) #Muestro Umbral
+    contorno, jerarquía = cv2.findContours(umbral, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0] #Contornos
+    contorno = sorted(contorno, key=cv2.contourArea, reverse=True) #Ordeno las matrices (los puntos) de menor a mayor
+    #Con este bucle recorro todos los contornos encontrados
+    for c in contorno:
+        epsilon = 0.01*cv2.arcLength(c, True) #Cierro los contornos encontrados y recorridos
+        aproximacion = cv2.approxPolyDP(c, epsilon, True) #Aproximacion
+        if len(aproximacion)==4: #Si ha encontrado la aproximacion 4 puntos del circulo...
+            puntos = ordenarPuntos(aproximacion)
+            puntos1 = np.float32(puntos)
+            puntos2 = np.float32([[0,0],[ancho,0],[0,alto],[ancho,alto]])
+            #Perspectiva, por si muevo la camara o algo
+            M = cv2.getPerspectiveTransform(puntos1, puntos2)
+            imagen_alineada = cv2.warpPerspective(imagen, M, (ancho,alto))
